e26c61ca90ec3653345462b7af538f3a8b2ed4cb
MenBati <chatdeau@protonmail.com>
2023-03-07 09:33:49 +0100

    Fix source region bretagne

--- scripts/sources/region-bretagne/convert.sh
+++ scripts/sources/region-bretagne/convert.sh
@@ -10,7 +10,8 @@ mkdir -p $DECP_HOME/json/$source
 
 for xml in *.xml
 do
-
+#fix xml malformes contenant des &
+sed -i 's/&/&amp;/g' $xml
 #Convertit le XML DECP vers JSON DECP
 $DECP_HOME/scripts/xmlDECP2jsonDECP.sh $xml > $DECP_HOME/json/$source/$xml.json
 done
--- scripts/sources/region-bretagne/get.sh
+++ scripts/sources/region-bretagne/get.sh
@@ -4,4 +4,11 @@
 # Récupération des données
 
 
-wget -q -O decp2021_regionbretagne.xml https://data.bretagne.bzh/api/datasets/1.0/decp-crb/alternative_exports/decp2021_regionbretagne_csv_xml
+echo "Telechargement region bretagne 2019"
+wget -q -O decp2019_regionbretagne.xml https://data.bretagne.bzh/api/datasets/1.0/decp-crb0/alternative_exports/2019_decp_crb_xml
+echo "Telechargement region bretagne 2020"
+wget -q -O decp2020_regionbretagne.xml https://data.bretagne.bzh/api/datasets/1.0/decp-crb0/alternative_exports/2020_decp_crb_xml
+echo "Telechargement region bretagne 2021"
+wget -q -O decp2021_regionbretagne.xml https://data.bretagne.bzh/api/datasets/1.0/decp-crb0/alternative_exports/2021_decp_crb_xml
+echo "Telechargement region bretagne 2022"
+wget -q -O decp2022_regionbretagne.xml https://data.bretagne.bzh/api/datasets/1.0/decp-crb0/alternative_exports/2022_decp_crb_xml
